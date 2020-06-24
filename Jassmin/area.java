1d27926848b2b75a0e0f594dd34af0916b75fdd7
golf34711 <67311098+golf34711@users.noreply.github.com>
2020-06-24 18:35:47 +0700

    Add files via upload

---
+++ Jassmin/area.java
@@ -0,0 +1,15 @@
+//snippets
+
+/**
+ * area
+ */
+public class area {
+
+public static double rectangle(double w, double h) {
+    return w * h;    
+}
+    public static void main(String[] args) {
+        System.out.println(rectangle(10, 20));
+        System.out.println(rectangle(15, 30));
+    }
+}
---
+++ Jassmin/areaapp.java
@@ -0,0 +1,10 @@
+/**
+ * areaapp
+ */
+public class areaapp {
+
+        public static void main(String[] args) {
+            double a1 = area.rectangle(5, 2);
+            System.out.println(a1);
+        }
+}
